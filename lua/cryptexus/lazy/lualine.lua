e77a1ef4d01f6003656d4b0f25103e6e9ced05a0
Maxime Aubaret <maxime.aubaret@gmail.com>
2025-03-07 14:44:55 +0100

    Add macro recording status in lualine

--- lua/cryptexus/lazy/lualine.lua
+++ lua/cryptexus/lazy/lualine.lua
@@ -6,7 +6,19 @@ return {
       lualine_a = { 'mode' },
       lualine_b = { 'branch', 'diff', 'diagnostics' },
       lualine_c = {
-        { 'filename', path = 1 }
+        { 'filename', path = 1 },
+        {
+          'macro',
+          fmt = function()
+            local reg = vim.fn.reg_recording()
+            if reg ~= "" then
+              return "Recording @" .. reg
+            end
+            return nil
+          end,
+          color = { fg = "#ff9e64" },
+          draw_empty = false,
+        },
       },
       lualine_x = { 'encoding', 'fileformat', 'filetype' },
       lualine_y = { 'progress' },
