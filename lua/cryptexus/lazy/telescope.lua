8e21bed1db80d7ca3db5d62d33d996afd0586d9a
Maxime Aubaret <maxime.aubaret@gmail.com>
2025-03-23 06:00:56 +0100

    gitsigns, supermaven

--- lua/cryptexus/lazy/telescope.lua
+++ lua/cryptexus/lazy/telescope.lua
@@ -1,36 +1,36 @@
 return {
-  "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-  tag = "0.1.5",
+	tag = "0.1.5",
 
-  dependencies = {
-    "nvim-lua/plenary.nvim"
-  },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 
-  config = function()
-    require('telescope').setup({
-      defaults = {
-        mappings = {
-          i = {
-            ["<esc>"] = require("telescope.actions").close,
-            ["<C-k>"] = require("telescope.actions").move_selection_previous,
-            ["<C-j>"] = require("telescope.actions").move_selection_next,
-          }
-        }
-      }
-    })
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<esc>"] = require("telescope.actions").close,
+						["<C-k>"] = require("telescope.actions").move_selection_previous,
+						["<C-j>"] = require("telescope.actions").move_selection_next,
+					},
+				},
+			},
+		})
 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader><leader>', builtin.git_files, { desc = 'Find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
-    vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = 'Commands' })
-    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Keymaps' })
-    vim.keymap.set('n', '<leader>fu', builtin.colorscheme, { desc = 'Keymaps' })
-    vim.keymap.set('n', '<leader>f?', builtin.man_pages, { desc = 'Man Pages' })
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+		vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymaps" })
+		vim.keymap.set("n", "<leader>fu", builtin.colorscheme, { desc = "Keymaps" })
+		vim.keymap.set("n", "<leader>f?", builtin.man_pages, { desc = "Man Pages" })
 
-    vim.keymap.set('n', '<leader>cfs', builtin.lsp_dynamic_workspace_symbols, { desc = 'Symbols' })
-    vim.keymap.set('n', '<leader>cfr', builtin.lsp_references, { desc = 'References' })
-  end
+		vim.keymap.set("n", "<leader>cfs", builtin.lsp_dynamic_workspace_symbols, { desc = "Symbols" })
+		vim.keymap.set("n", "<leader>cfr", builtin.lsp_references, { desc = "References" })
+	end,
 }
