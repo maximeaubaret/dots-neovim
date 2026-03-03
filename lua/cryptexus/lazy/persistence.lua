babc4e3e7c5fc3028c450b08b851788ebba5ab10
Maxime Aubaret <maxime.aubaret@gmail.com>
2026-03-03 12:52:20 +0100

    feat(persistence): auto-save session on FocusLost

--- lua/cryptexus/lazy/persistence.lua
+++ lua/cryptexus/lazy/persistence.lua
@@ -1,7 +1,16 @@
 return {
 	"folke/persistence.nvim",
 	event = "BufReadPre", -- this will only start session saving when an actual file was opened
-	opts = {
-		-- add any custom options here
-	},
+	opts = {},
+	config = function(_, opts)
+		local persistence = require("persistence")
+		persistence.setup(opts)
+
+		vim.api.nvim_create_autocmd("FocusLost", {
+			group = vim.api.nvim_create_augroup("PersistenceAutoSave", { clear = true }),
+			callback = function()
+				persistence.save()
+			end,
+		})
+	end,
 }
